b30cae994f8160049d0f90d559f536ffd22e5f2d
ogustavodias <gustavoalvesdiaz08@gmail.com>
2024-10-07 19:47:58 -0300

    feat: testes e métodos da classe ProductDAO implementados

--- src/test/CustomerTest.java
+++ src/test/CustomerTest.java
@@ -4,48 +4,60 @@ import java.sql.SQLException;
 import java.util.List;
 
 import org.junit.Assert;
-import org.junit.FixMethodOrder;
 import org.junit.Test;
-import org.junit.runners.MethodSorters;
 
 import main.db.CustomerDAO;
 import main.db.ICustomerDAO;
 import main.domain.Customer;
 
-@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class CustomerTest {
 
   private ICustomerDAO dao = new CustomerDAO();
 
+  public int insertBefore() throws SQLException {
+    Customer customer = new Customer("48587815857", "Gustavo Alves Dias");
+    return dao.register(customer);
+  }
+
+  public int deleteAfter() throws SQLException {
+    return dao.deleteCustomer("48587815857");
+  }
+
   @Test
   public void register() throws SQLException {
-    Customer customer = new Customer("48587815857", "Gustavo Alves Dias");
-    Assert.assertTrue(dao.register(customer) > 0);
+    Assert.assertTrue(insertBefore() > 0);
   }
 
   @Test
   public void search() throws SQLException {
+    insertBefore();
     Customer customerDB = dao.search("48587815857");
     Assert.assertNotNull(customerDB);
     Assert.assertEquals(customerDB.getCpf(), "48587815857");
+    deleteAfter();
   }
 
   @Test
   public void searchAll() throws SQLException {
+    insertBefore();
     List<Customer> customerList = dao.searchAll();
     Assert.assertTrue(customerList.size() > 0);
+    deleteAfter();
   }
 
   @Test
   public void updateCustomer() throws SQLException {
+    insertBefore();
     Customer customer = new Customer("48587815857", "TESTE UPDATE");
     Boolean validation = dao.updateCustomer(customer) > 0 && dao.search("48587815857").getNome().equals("TESTE UPDATE");
     Assert.assertTrue(validation);
+    deleteAfter();
   }
 
   @Test
   public void deleteCustomer() throws SQLException {
-    Boolean validation = dao.deleteCustomer("48587815857") > 0 && dao.search("48587815857") == null;
+    insertBefore();
+    Boolean validation = deleteAfter() > 0 && dao.search("48587815857") == null;
     Assert.assertTrue(validation);
   }
 
--- src/test/ProductTest.java
+++ src/test/ProductTest.java
@@ -5,48 +5,60 @@ import java.sql.SQLException;
 import java.util.List;
 
 import org.junit.Assert;
-import org.junit.FixMethodOrder;
 import org.junit.Test;
-import org.junit.runners.MethodSorters;
 
 import main.db.IProductDAO;
 import main.db.ProductDAO;
 import main.domain.Product;
 
-@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class ProductTest {
 
   private IProductDAO dao = new ProductDAO();
 
+  public int insertBefore() throws SQLException {
+    Product product = new Product("1", "Iphone 13", new BigDecimal(1400.99));
+    return dao.register(product);
+  }
+
+  public int deleteAfter() throws SQLException {
+    return dao.deleteProduct("1");
+  }
+
   @Test
   public void register() throws SQLException {
-    Product product = new Product("1", "Iphone 13", new BigDecimal(1400.99));
-    Assert.assertTrue(dao.register(product) > 0);
+    Assert.assertTrue(insertBefore() > 0);
   }
 
   @Test
   public void search() throws SQLException {
+    insertBefore();
     Product productDB = dao.search("1");
     Assert.assertNotNull(productDB);
     Assert.assertEquals(productDB.getCodigo(), "1");
+    deleteAfter();
   }
 
   @Test
   public void searchAll() throws SQLException {
+    insertBefore();
     List<Product> productList = dao.searchAll();
     Assert.assertTrue(productList.size() > 0);
+    deleteAfter();
   }
 
   @Test
   public void updateProduct() throws SQLException {
+    insertBefore();
     Product product = new Product("1", "TESTE UPDATE", new BigDecimal(1400.99));
     Boolean validation = dao.updateProduct(product) > 0 && dao.search("1").getNome().equals("TESTE UPDATE");
     Assert.assertTrue(validation);
+    deleteAfter();
   }
 
   @Test
   public void deleteProduct() throws SQLException {
-    Boolean validation = dao.deleteProduct("1") > 0 && dao.search("1") == null;
+    insertBefore();
+    Boolean validation = deleteAfter() > 0 && dao.search("1") == null;
     Assert.assertTrue(validation);
   }
 
